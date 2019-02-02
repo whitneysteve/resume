7258ca88452ecc193a7a7aaa9bce9997ab6cb501
Stephen Whitney <steve.whitney@gmail.com>
2019-02-02 20:45:31 +0000

    Make the landing simpler

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,4 +1,4 @@
-import Floaty from '../floaty/Floaty'
+import CthuluColumns from '../floaty/CthuluColumns'
 import React, { Component } from 'react';
 
 /**
@@ -8,16 +8,11 @@ class Landing extends Component {
   render() {
     return (
       <div className="Section" id="landing">
-        <h1>Intro</h1>
-        <div className="avatar">
-            <img
-              alt="Stephen Whitney"
-              src="https://www.gravatar.com/avatar/56ca7097c63f89715d830034165ff7bd?s=350"/>
-        </div>
-        <h2>Stephen Whitney</h2>
-        <h3>Software Engineer</h3>
-        <h4>Since 2005</h4>
-        <Floaty numLayers={3} />
+          <h1>Intro</h1>
+          <h2>Stephen Whitney</h2>
+          <h3>Software Engineer</h3>
+          <h4>Since 2005</h4>
+          <CthuluColumns />
       </div>
     );
   }
