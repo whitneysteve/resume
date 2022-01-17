cd5ae3874fefc6866be88d0eec9d264ec7187fef
Steve Whitney <steve.whitney@gmail.com>
2022-01-17 15:45:07 +0000

    extra-employment-info

--- src/experience/Experience.js
+++ src/experience/Experience.js
@@ -4,56 +4,76 @@ import intercom from '../img/companies/intercom.svg';
 import stripe from '../img/companies/stripe.svg';
 import twitter from '../img/companies/twitter.svg';
 import mastercard from '../img/companies/mastercard.svg';
+import PropTypes from 'prop-types';
 
 /**
  * Experience renders my previous jobs.
  */
 class Experience extends Component {
+  static defaultProps = {
+    expanded: false,
+    selectedJob: null,
+  };
+
+  state = { expanded: this.props.expanded, selectedJob: this.props.selectedJob };
+
   componentDidMount() {
     // Stripe gradient provided by https://whatamesh.vercel.app
     const gradient = new Gradient();
     gradient.initGradient('#gradient-canvas-stripe');
+
+    this.selectJob = this.selectJob.bind(this);
+  }
+
+  /**
+   * Genreate the markup for a job and attach hover events to update the
+   * background.
+   *
+   * @param {Object} job the job configuration.
+   */
+  genJob = (job) => {
+    const containerId = `experience-job-${job.id}`;
+    const selectedState = this.state.selectedJob === job ? 'selected' : '';
+
+    return (
+      <div className="dev-grid-cell experience-job" id={ job.id } key={ job.id }>
+        <div className="dev-grid-margin-top-left-corner-left" data-target={containerId}></div>
+        <div className="dev-grid-margin-center-up" data-target={containerId}></div>
+        <div id={containerId} className={`experience-job-company-container ${containerId}  ${selectedState}`} onClick={() => this.selectJob(job)} >
+          <img alt={ `${ job.company } logo` } src={ job.logo } />
+          <div className="experience-job-description">
+            <div className="experience-job-description-company-name">
+              {job.company}
+            </div>
+            <div className="experience-job-description-company-term">
+              {job.term}
+            </div>
+          </div>
+          <canvas id={`gradient-canvas-${job.id}`} data-transition-in />
+        </div>
+      </div>
+    );
+  }
+
+  selectJob(job) {
+    this.setState({ selectedJob: job });
   }
 
   render() {
     return (
       <div className="Section experience dev-grid" id="experience">
-        { JOBS.map(genJob) }
+        { JOBS.map(this.genJob) }
       </div>
     );
   }
 }
 
-/**
- * Genreate the markup for a job and attach hover events to update the
- * background.
- *
- * @param {Object} job the job configuration.
- */
-const genJob = (job) => {
-  const jobId = job.company.toLowerCase();
-  const containerId = `experience-job-${jobId}`;
-
-  return (
-    <div className="dev-grid-cell experience-job" id={ job.id } key={ jobId }>
-      <div className="dev-grid-margin-top-left-corner-left" data-target={containerId}></div>
-      <div className="dev-grid-margin-center-up" data-target={containerId}></div>
-      <div id={containerId} className={`experience-job-company-container ${containerId}`}>
-        <img alt={ `${ job.company } logo` } src={ job.logo } />
-        <canvas id={`gradient-canvas-${jobId}`} data-transition-in />
-      </div>
-    </div>
-  );
-};
-
-const JOBS = [
+export const JOBS = [
   {
     id: "intercom",
     company: "Intercom",
     position: "Software Engineer",
-    terms: [
-      "2021 - Present",
-    ],
+    term: `2021 - ${new Date(Date.now()).getFullYear()}`,
     blurbs: [
       "I recently joined Intercom to work on their Growth teams.",
     ],
@@ -63,9 +83,7 @@ const JOBS = [
     id: "stripe",
     company: "Stripe",
     position: "Software Engineer",
-    terms: [
-      "2018 - 2021",
-    ],
+    term: "2018 - 2021",
     blurbs: [
       "Working as a full-stack engineer on Stripe's Security Products, building great products to keep our users safe.",
       "At Stripe I've been fortunate enough to work between writing distributed services and message consumers in Ruby and Java and building, great user experiences in React.",
@@ -76,9 +94,7 @@ const JOBS = [
     id: "twitter",
     company: "Twitter",
     position: "Software Engineer",
-    terms: [
-      "2013 - 2018",
-    ],
+    term: "2013 - 2018",
     blurbs: [
       "Twitter is my favourite website and I’m very lucky to have had the opportunity to work and learn here.",
       "Mostly backend engineering on low and high throughput applications, from Ruby to Scala, from monoliths to micro services, and Hadoop.",
@@ -88,11 +104,9 @@ const JOBS = [
   },
   {
     id: "mastercard",
-    company: "MasterCard Labs",
+    company: "MC Labs",
     position: "Consultant Software Engineer",
-    terms: [
-      "2012 - 2013",
-    ],
+    term: "2012 - 2013",
     blurbs: [
       "Researching emerging technologies and trends to create prototypes and other innovations for MasterCard’s next and future generation of products.",
       "Had to pleasure of launching several new, multi-platform services to prototype, pilot, beta and full production.",
@@ -100,5 +114,10 @@ const JOBS = [
     logo: mastercard,
   },
 ];
+
+Experience.propTypes = {
+  expanded: PropTypes.bool,
+  selectedJob: PropTypes.object,
+};
 
 export default Experience;
