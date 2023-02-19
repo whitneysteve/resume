aa3045bab52758907264bac4af5337e328c8b3c1
Steve Whitney <steve.whitney@gmail.com>
2023-02-19 19:25:33 +0000

    full-scren-experience

--- src/app/App.js
+++ src/app/App.js
@@ -11,6 +11,14 @@ import Skills from '../skills/Skills';
  * The main driver and parent container for the app.
  */
 class App extends Component {
+  componentDidMount() {
+    const appHeight = () => {
+      document.documentElement.style.setProperty("--app-height", `${window.innerHeight}px`);
+    };
+    window.addEventListener("resize", appHeight);
+    appHeight();
+  }
+
   render() {
     return (
       <div className="App" id="App">
