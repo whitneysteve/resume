d7999a1a28648b7ae5241cd83438f1de092cf9f2
Steve Whitney <steve.whitney@gmail.com>
2023-04-26 17:15:23 +0100

    fix-issues

--- src/app/App.js
+++ src/app/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         <Experience />
         <Skills />
         <Learning />
-        {/* <Education /> */}
+        <Education />
         <Contact />
       </div>
     );
